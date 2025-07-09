c59c174137b38a997af8ff76108c78a6c056c19e
Victor Nguyo <victor.nguyo@student.moringaschool.com>
2025-07-09 22:38:40 +0300

    addeed functionality to toggle 'small-sidebar' and 'large-container' classes when the menu icon is clicked

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ let sidebar = document.querySelector(".side-bar");
 
 menuIcon.onclick = function() {
     sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
 }
